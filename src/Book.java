1543c76c32e7c66268da63329024afa42358bae7
jesus <jesus.vazquez@lcieducation.net>
2024-04-02 15:04:30 -0400

    2024-04-02 Init

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,55 @@
 public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private double price;
+    private int quantity;
+
+    public Book(String title, String author, String isbn, double price, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
